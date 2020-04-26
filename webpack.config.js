812e78fcd874b7b72b6a19c3d448fe5bd1d1a9f0
Stephan <s.bussing@gmail.com>
2020-04-26 15:22:05 +0200

    Save progress to GitHub

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const postCSSPlugins = [
   require("postcss-simple-vars"),
   require("postcss-nested"),
   require("autoprefixer"),
+  require("postcss-import"),
+  require("postcss-nested-import"),
 ];
 
 module.exports = {
